562a78b852a9db6a9f3ee07d324851d9e6f0fffa
diegoG82 <diegogiardina@hotmail.it>
2023-05-09 09:48:14 +0200

    sistemato file server php

--- server.php
+++ server.php
@@ -1,12 +1,12 @@
 <?php
 $string = file_get_contents("dischi.json");
-$dischi_array = json_decode($string);
+$dischi_array = json_decode($string, true);
 
 
 // Preparazione della risposta
 $response = [
     "results" => $dischi_array,
-];
-
+    ];
+// Invio della risposta
 header('Content-Type: application/json');
 echo json_encode($response);
